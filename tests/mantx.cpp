8ac2ad19bdf66891b4757aa7522a1be93220c27c
Juan Leni <lenijuan@gmail.com>
2019-05-31 08:44:56 +0200

    updating unit tests

--- tests/mantx.cpp
+++ tests/mantx.cpp
@@ -30,9 +30,7 @@ using ::testing::Values;
 struct MANTxTestCase {
     const char *name;
     const char *data;
-//    uint16_t expectedFieldCount;
-//    const char *expectedFields;
-//    const char *expectedValueStr;
+    const char *expectedStr;
 };
 
 class MANTxParamTest : public testing::TestWithParam<MANTxTestCase> {
@@ -50,7 +48,7 @@ INSTANTIATE_TEST_SUITE_P(
     InstantiationName,
     MANTxParamTest,
     Values(
-        MANTxTestCase{"exampleTx",
+        MANTxTestCase{"exampleTx1",
                       "f901e1"  // Root List
                       "80" // Nonce
                       "850430e23400" // Gas Price
@@ -66,14 +64,56 @@ INSTANTIATE_TEST_SUITE_P(
                       "7374476173223a747275652c224973456e74727573745369676e223a66616c73652c2253746172744865696768"
                       "74223a3132323232332c22456e64486569676874223a3132323232392c22456e73747275737453657454797065"
                       "223a302c22757365537461727454696d65223a22222c22757365456e6454696d65223a22222c22456e74727573"
-                      "74436f756e74223a307d5d"
+                      "74436f756e74223a307d5d"   // Data
                       "03" // ChainID
                       "80" // R
                       "80" // S
                       "80" // Enter Type
                       "80" // IsEntrust
                       "86016850894a0f" // Commit Time
-                      "c4c30580c0"
+                      "c4c30580c0", // ExtraTo
+            //////////// expected output:
+                      "Nonce = 0\n"
+                      "Gas Price = 18000000000\n"
+                      "Gas Limit = 210000\n"
+                      "To = MAN.Wkbujtxh7YBnkGV8HZvyPQK3cAPy\n"
+                      "Value = 10000000\n"
+                      "Data = [{\"EntrustAddres\":\"MAN.2Uoz8g8jauMa2mtnwxrschj2qPJrE\",\"IsEntrustGas\":true,\"IsEntrustSign\":false,\"StartHeight\":1222,\"EndHeight\":122222,\"EnstrustSetType\":0,\"useStartTime\":\"\",\"useEndTime\":\"\",\"EntrustCount\":0},{\"EntrustAddres\":\"MAN.2Uoz8g8jauMa2mtnwxrschj2qPJrE\",\"IsEntrustGas\":true,\"IsEntrustSign\":false,\"StartHeight\":122223,\"EndHeight\":122229,\"EnstrustSetType\":0,\"useStartTime\":\"\",\"useEndTime\":\"\",\"EntrustCount\":0}]\n"
+                      "ChainID = 3\n"
+                      "EnterType = 0\n"
+                      "IsEntrustTx = 0\n"
+                      "CommitTime = 15Jan2019 08:03:21\n"
+                      "ExtraTo = \n"
+        },
+        MANTxTestCase{"exampleTx2",
+                      "f8c287"
+                      "10000000000039"
+                      "850430e23400"
+                      "83033450"
+                      "a14d414e2e32556f7a3867386a61754d61326d746e7778727363686a3271504a7245"
+                      "83989680"
+                      "80"
+                      "03"
+                      "80"
+                      "80"
+                      "80"
+                      "80"
+                      "86016850894a0ff87bf8798080f875e6a04d414e2e6a4c5446686f434a43474368706964553269433151"
+                      "357a436d56464c8398968080e6a04d414e2e66344657484562576b583873536438796a5a6a5948655a57"
+                      "6e6164788398968080e6a04d414e2e675141414855655442787667627a6638744667557461764463654a"
+                      "508398968080",
+            //////////// expected output:
+                      "Nonce = 4503599627370553\n"
+                      "Gas Price = 18000000000\n"
+                      "Gas Limit = 210000\n"
+                      "To = MAN.2Uoz8g8jauMa2mtnwxrschj2qPJrE\n"
+                      "Value = 10000000\n"
+                      "Data = \n"
+                      "ChainID = 3\n"
+                      "EnterType = 0\n"
+                      "IsEntrustTx = 0\n"
+                      "CommitTime = 15Jan2019 08:03:21\n"
+                      "ExtraTo = \n"
         }
     ),
     MANTxParamTest::PrintToStringParamName()
@@ -118,15 +158,20 @@ TEST_P(MANTxParamTest, iterateDisplayStream) {
     auto err = mantx_parse(&ctx, data, dataSize);
     EXPECT_THAT(err, testing::Eq(MANTX_NO_ERROR));
 
+    std::stringstream ss;
     std::cout << std::endl;
 
     char displayKey[1000];
     char displayValue[1000];
 
-    for (int8_t displayIdx = 0; displayIdx < 13; displayIdx++) {
+    for (int8_t displayIdx = 0; displayIdx < MANTX_DISPLAY_COUNT; displayIdx++) {
         err = mantx_getItem(&ctx, data, displayIdx, displayKey, 1000, displayValue, 1000);
         if (err == MANTX_NO_ERROR) {
-            std::cout << displayKey << " = " << displayValue << std::endl;
+            ss << displayKey << " = " << displayValue << std::endl;
         }
     }
+
+    std::cout << ss.str() << std::endl;
+
+    EXPECT_THAT(ss.str(), testing::Eq(params.expectedStr));
 }
